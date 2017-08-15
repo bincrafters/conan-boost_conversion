ecac5c7ed1d64d7ae1c8ce0810642d8e8b91c52d
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:09 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostConversionConan(ConanFile):
     name = "Boost.Conversion"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/conversion"
+    url = "https://github.com/bincrafters/conan-boost-conversion"
+    source_url = "https://github.com/boostorg/conversion"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     requires =  "Boost.Level5Group/1.64.0@bincrafters/testing"
